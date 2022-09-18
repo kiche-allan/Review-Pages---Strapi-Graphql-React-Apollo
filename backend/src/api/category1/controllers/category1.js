0db65861e384b23ea09e66d05b3d586ac09dd381
allan Kiche <allankiche18@gmail.com>
2022-09-19 00:53:26 +0300

    first commit

---
+++ backend/src/api/category1/controllers/category1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category1 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::category1.category1');
---
+++ backend/src/api/category1/routes/category1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category1 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::category1.category1');
